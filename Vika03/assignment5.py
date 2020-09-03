f32105cc95d1be3333d2b87d9e71adb9f26fc85a
BaldvinAri <baldvin19@ru.is>
2020-09-03 14:42:43 +0000

    skil1

--- Vika03/assignment5.py
+++ Vika03/assignment5.py
@@ -1,8 +1,12 @@
 num_int = int(input("Input a number: "))    # Do not change this line
-# Fill in the missing code
-print("The maximum is", max_int)    # Do not change this line
-print("The maximum is", max_int)    # Do not change this line
-print("The maximum is", max_int)    # Do not change this line
-print("The maximum is", max_int)    # Do not change this line
-print("The maximum is", max_int)    # Do not change this line
-print("The maximum is", max_int)    # Do not change this line
+max_int = 0
+while -1 < num_int:
+
+    if num_int >= max_int:
+        max_int = num_int
+
+    print("The maximum is", max_int)    # Do not change this line
+    num_int = int(input("Input a number: "))
+
+# Design an algorithm that finds the maximum positive integer input by a user.  
+# The user repeatedly inputs numbers until a negative value is entered.
