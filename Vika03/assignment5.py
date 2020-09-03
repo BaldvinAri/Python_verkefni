0c2b562901df591fe8b484752b9f501ee90f85d1
BaldvinAri <baldvin19@ru.is>
2020-09-03 14:31:59 +0000

    ég breytti engu

--- Vika03/assignment5.py
+++ Vika03/assignment5.py
@@ -1,3 +1,8 @@
 num_int = int(input("Input a number: "))    # Do not change this line
 # Fill in the missing code
 print("The maximum is", max_int)    # Do not change this line
+print("The maximum is", max_int)    # Do not change this line
+print("The maximum is", max_int)    # Do not change this line
+print("The maximum is", max_int)    # Do not change this line
+print("The maximum is", max_int)    # Do not change this line
+print("The maximum is", max_int)    # Do not change this line
