0a4697c40bc202e39ba8e8399e6b3e17323344b3
BaldvinAri <baldvin19@ru.is>
2020-09-10 17:48:18 +0000

    prufan

--- Vika 04/prufavika4.py
+++ Vika 04/prufavika4.py
@@ -1,6 +1,15 @@
-max_int = int(input("Input max integer: "))
+my_int = int(input('Give me an int >= 0: '))
+quot_int = my_int
+bin_str = ''
 
-for i in range(1,max_int + 1):
-    for n in range(1,i + 1):
-        print(n,end = ' ')
-    print()
+while quot_int > 0:
+
+    bin_str += str(quot_int % 2)
+    quot_int = quot_int // 2
+    print(quot_int)
+    print(bin_str)
+
+bin_str = bin_str[::-1]
+
+
+print("The binary of {} is {}".format(my_int,bin_str))
